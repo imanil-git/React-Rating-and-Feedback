6789e514799ac39cf824e4dfe3e5ffbb81cdf6dd
Anil Rumdali Rai <anil.rai704757@gmail.com>
2025-02-04 19:30:36 +0545

    Modal Added

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
